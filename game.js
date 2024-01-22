0e9a6286be505683642477781d381dfc88a44973
Jared Gleisner <darkphoton31@gmail.com>
2024-01-22 09:47:35 -0600

    removed a theoretically superfulous line where it was loading the fish gif again. This may also fix the reset timing button, although I don't know why it would

--- game.js
+++ game.js
@@ -441,7 +441,7 @@ loadthings = function() {
 		//alert(document.getElementById('bottom_thingy').clientHeight)
 	}
 
-	document.getElementById(mobileprefix+'fish').src = 'sprites/spinning_fish_transparent.gif';
+	//document.getElementById(mobileprefix+'fish').src = 'sprites/spinning_fish_transparent.gif';
 
 	gamestarted = true;
 	
